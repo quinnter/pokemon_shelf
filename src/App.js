61fef70718db05c3504fb7af203c9cb8e9114f15
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-06-18 12:43:36 +0100

    created axios get request to pokeapi

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import SearchForm from './components/SearchForm';
+import PokemonGrid from './components/PokemonGrid';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    userSearchTerm: ""
+    userSearchTerm: "",
+    pokemon: []
   }
   render() {
+    const { pokemon } = this.state
     return (
       <div className="App">
         <Header />
         <SearchForm setSearchTerm={this.setSearchTerm} />
+        <PokemonGrid pokemon={pokemon} />
       </div>
     );
+  }
+
+  componentDidMount = () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon-species'
+    axios.get(url)
+      .then(({ data: { results } }) => {
+        this.setState({ pokemon: results })
+      })
   }
 
   setSearchTerm = userInput => {
---
+++ src/components/PokemonGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonGrid = () => {
+    return (
+        <div>
+            <p>A pokemon</p>
+        </div>
+    )
+}
+
+export default PokemonGrid
