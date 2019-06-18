ec6dd59be4ca8d05899af1f3f8cf1eadd57db046
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-06-18 12:16:03 +0100

    set up search form component added it to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchForm />
     </div>
   );
 }
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SearchForm = () => {
+    return (
+        <div>
+            <form>
+                <input>
+
+                </input>
+                <button>Search...</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchForm
